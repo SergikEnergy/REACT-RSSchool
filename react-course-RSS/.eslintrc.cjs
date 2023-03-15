42fba1bb2fce853169af44886d243bd0436643ba
SergEnergy <kis8883244@gmail.com>
2023-03-15 23:54:11 +0300

    init: create config ts vite react es;int prettier air-bnb

---
+++ react-course-RSS/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    indent: ['warn', 2],
+    'max-len': ['warn', { code: 120 }],
+    semi: ['warn', 'always'],
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'only-multiline'],
+  },
+};
