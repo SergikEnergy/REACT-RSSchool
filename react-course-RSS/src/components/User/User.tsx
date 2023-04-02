8fdde44723038d3ddf8728cff15f32a422bee2a0
SergEnergy <kis8883244@gmail.com>
2023-04-02 14:32:08 +0300

    feat: rewrite user and userList to FC React

--- react-course-RSS/src/components/User/User.tsx
+++ react-course-RSS/src/components/User/User.tsx
@@ -1,31 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { IUserCard } from '../../types';
 
 import './user.css';
 
 interface UserProps {
-  key: string;
   data: IUserCard;
 }
 
-// eslint-disable-next-line react/prefer-stateless-function
-export default class User extends Component<UserProps> {
-  render() {
-    const { data } = this.props;
-
-    return (
-      <div className="user-cards__item user" data-testid="singleUser">
-        <img
-          data-testid="userImg"
-          className="user__avatar"
-          src={data.image}
-          alt={`${data.name} avatar`}
-        />
-        <p className="user__info">Name: {data.name}</p>
-        <p className="user__info">Last Name: {data.lastName}</p>
-        <p className="user__info">Birthday: {data.birthDay}</p>
-        <p className="user__info">Favorite meal: {data.meal}</p>
-      </div>
-    );
-  }
+export default function User(props: UserProps) {
+  const { data } = props;
+  return (
+    <div className="user-cards__item user" data-testid="singleUser">
+      <img
+        data-testid="userImg"
+        className="user__avatar"
+        src={data.image}
+        alt={`${data.name} avatar`}
+      />
+      <p className="user__info">Name: {data.name}</p>
+      <p className="user__info">Last Name: {data.lastName}</p>
+      <p className="user__info">Birthday: {data.birthDay}</p>
+      <p className="user__info">Favorite meal: {data.meal}</p>
+    </div>
+  );
 }
--- react-course-RSS/src/components/UsersCardList/UsersCardList.tsx
+++ react-course-RSS/src/components/UsersCardList/UsersCardList.tsx
@@ -4,19 +4,13 @@ import { UsersCardListProps } from '../../types';
 import User from '../User/User';
 import './usersCardList.css';
 
-// eslint-disable-next-line react/prefer-stateless-function
-export default class UsersCardList extends React.Component<
-  UsersCardListProps,
-  unknown
-> {
-  render() {
-    const { cards } = this.props;
-    return (
-      <div className="users-wrapper" data-testid="TestUserListWrapper">
-        {cards.map((user) => {
-          return <User key={user.id} data={{ ...user }} />;
-        })}
-      </div>
-    );
-  }
+export default function UsersCardList(props: UsersCardListProps) {
+  const { cards } = props;
+  return (
+    <div className="users-wrapper" data-testid="TestUserListWrapper">
+      {cards.map((user) => {
+        return <User key={user.id} data={{ ...user }} />;
+      })}
+    </div>
+  );
 }
