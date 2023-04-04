045738c695193822dedf08591416714ecff92655
SergEnergy <kis8883244@gmail.com>
2023-04-05 00:28:43 +0300

    fix: change file after crosscheck one of the schoolmates

--- react-course-RSS/src/components/UsersCardList/UserCardList.test.tsx
+++ react-course-RSS/src/components/UsersCardList/UserCardList.test.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { describe, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { IUserCard } from '../../types';
--- react-course-RSS/vite.config.ts
+++ react-course-RSS/vite.config.ts
@@ -8,6 +8,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxInject: `
+import React from 'react';
+`,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
